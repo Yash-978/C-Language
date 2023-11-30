de0ce61fd4b37730de57f11a987bb3b2221367f1
Yash <147479013+Yash-978@users.noreply.github.com>
2023-11-30 16:59:31 +0530

    Add files via upload

---
+++ Pattern with loop/pls 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	for(a=1;a<=5;a++)
+	{
+		for(c=a;c<5;c++)
+		{
+			printf(" ",c);
+		}
+		for(b=1;b<=a;b++)
+		{
+			printf("%d",b);	
+		}	
+		printf("\n");
+		
+	}
+}
