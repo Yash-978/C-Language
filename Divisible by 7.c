35fd339270c83b9add13cf38c030143fe2d66f9b
Yash <147479013+Yash-978@users.noreply.github.com>
2023-12-17 11:37:52 -0800

    Delete Divisible by 7.c

--- Divisible by 7.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main()
-{
-	int i;
-	printf("Enter the value :");
-	scanf("%d",&i);
-	(i%7==0)? printf("the value is divsible by 7.") : printf("the value is not divisible by 7.");
-
-}
