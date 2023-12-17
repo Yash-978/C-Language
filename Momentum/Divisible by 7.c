46b7d2580567667ffe7d6b93f6f9e767bcc13835
Yash <147479013+Yash-978@users.noreply.github.com>
2023-12-17 11:40:35 -0800

    Add files via upload

---
+++ Momentum/Divisible by 7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	int i;
+	printf("Enter the value :");
+	scanf("%d",&i);
+	(i%7==0)? printf("the value is divsible by 7.") : printf("the value is not divisible by 7.");
+
+}
